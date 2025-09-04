59f4c28dd1b89f0297dda78737a2d542fee21cc8
ARNAIM.COM <52743150+masfy@users.noreply.github.com>
2025-09-04 21:08:19 +0800

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoutesDef from "./routes.jsx";
+import Toast from "./components/Toast.jsx";
+
+export default function App() {
+  return (
+    <>
+      <RoutesDef />
+      <Toast />
+    </>
+  );
+}
